b21c5ef9be96f347d4e4071a6d4276c17188f726
VasilIvanovitch <vnechviadovich@gmail.com>
2023-04-21 18:11:41 +0300

    Обновление безопасности с locals_vars.py

--- store/settings.py
+++ store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from locals_vars import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0cm(agt+ea+zw%x*+2#kj2hhi7_ir7=*@oq)valj1zm3a92fgh'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

dbd725603b80d32f2a239ce7bb6b381523f82656
VasilIvanovitch <vnechviadovich@gmail.com>
2023-04-23 22:32:31 +0300

    CreateView, UpdateView внедрение классов, урок 7.6

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from users import views
-
+from django.contrib.auth.decorators import login_required
 app_name = 'users'
 
 urlpatterns = [
     #  path('', views.index, name='index'),
     path('login', views.UserLoginView.as_view(), name='login'),
     # path('login', views.login, name='login'),
-    path('registration', views.registration, name='registration'),
-    path('profile', views.profile, name='profile'),
+    path('registration', views.UserRegistrationView.as_view(), name='registration'),
+    # path('registration', views.registration, name='registration'),
+    path('profile/<int:pk>', login_required(views.UserProfileView.as_view()), name='profile'),
+    # path('profile', views.profile, name='profile'),
     path('logout', LogoutView.as_view(), name='logout'),
     # path('logout', views.logout, name='logout'),
 ]

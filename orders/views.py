64210f6499a0707179c70f316f5d494ba22d0dc4
VasilIvanovitch <vnechviadovich@gmail.com>
2023-05-02 18:39:02 +0300

    Созданы class OrdrerModel, OrderForm. урок 10.3

--- orders/views.py
+++ orders/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 
+from orders.forms import OrderForm
 
-class OrderCreateView(TemplateView):
-    # CreateView
+
+class OrderCreateView(CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm

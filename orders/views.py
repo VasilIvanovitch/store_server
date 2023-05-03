5a4f720c0f2fba94e2b8f4ff3f4420d9740f7ed6
Vasil <vnechviadovich@gmail.com>
2023-05-03 23:15:36 +0300

    Stripe - подключение webhook урок 10.6

--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,8 @@ from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy, reverse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 from orders.forms import OrderForm
 from common.views import TitleMixin
@@ -30,6 +32,7 @@ class OrderCreateView(TitleMixin, CreateView):
                     'quantity': 1,
                 },
             ],
+            metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_canceled')),
@@ -48,3 +51,77 @@ class SuccessTemplateView(TitleMixin, TemplateView):
 
 class CanceledTemplateView(TemplateView):
     template_name = 'orders/canceled.html'
+
+
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+
+def fulfill_order(session):
+    order_id = int(session.metadata.order_id)
+    print("Fulfilling order")
+
+# @csrf_exempt
+# def stripe_webhook_view(request):
+#     payload = request.body
+#     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+#     event = None
+#
+#     try:
+#         event = stripe.Webhook.construct_event(
+#             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+#         )
+#     except ValueError as e:
+#         # Invalid payload
+#         return HttpResponse(status=400)
+#     except stripe.error.SignatureVerificationError as e:
+#         # Invalid signature
+#         return HttpResponse(status=400)
+#
+#     # Handle the checkout.session.completed event
+#     if event['type'] == 'checkout.session.completed':
+#         # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+#         session = stripe.checkout.Session.retrieve(
+#             event['data']['object']['id'],
+#             expand=['line_items'],
+#         )
+#
+#         line_items = session.line_items
+#         # Fulfill the purchase...
+#         fulfill_order(line_items)
+#
+#     # Passed signature verification
+#     return HttpResponse(status=200)
+#
+#
+# def fulfill_order(session):
+#     order_id = int(session.metadata.order_id)
+#     print("Fulfilling order", order_id)
+
+# stripe listen --forward-to 127.0.0.1:8000/webhook/stripe/
+# id acct_1N3bkNBg6cT5HF9M
+# 4242 4242 4242 4242
